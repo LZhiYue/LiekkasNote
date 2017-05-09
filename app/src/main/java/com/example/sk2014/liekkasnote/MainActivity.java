2b3f128245e583686aa5258acecf80b936dc9b96
LZhiYue <q2912982@hotmail.com>
2017-05-09 14:36:10 +0800

    init application

---
+++ app/src/main/java/com/example/sk2014/liekkasnote/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sk2014.liekkasnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
