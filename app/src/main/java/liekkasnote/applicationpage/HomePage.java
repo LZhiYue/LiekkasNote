7da64e0358087c6f7f8939707b25d2898dfe6244
LZhiYue <q2912982@hotmail.com>
2017-06-08 15:26:06 +0800

    添加了主页，注册页面，添加了图标资源

---
+++ app/src/main/java/liekkasnote/applicationpage/HomePage.java
@@ -0,0 +1,15 @@
+package liekkasnote.applicationpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sk2014.liekkasnote.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
