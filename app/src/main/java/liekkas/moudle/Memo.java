c54ae894d3fc7be753b011b8abff14659997c67e
LZhiYue <q2912982@hotmail.com>
2017-06-12 13:12:46 +0800

    添加Diary，Memo，User 三个类

---
+++ app/src/main/java/liekkas/moudle/Memo.java
@@ -0,0 +1,12 @@
+package liekkas.moudle;
+
+/**
+ * Created by sk2014 on 2017/6/10.
+ */
+
+public class Memo {
+    private long id;
+    public String content;
+    public boolean is_done;
+    public String userId;
+}
