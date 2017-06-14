6066967158dad55151b0f6c79b3ebb66567a3c63
LZhiYue <q2912982@hotmail.com>
2017-06-14 22:47:06 +0800

    Revert "添加Diary，Memo，User 三个类"
    This reverts commit c54ae894d3fc7be753b011b8abff14659997c67e.
    
    # Conflicts:
    #	app/src/main/res/layout/activity_home_page.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/liekkasnote/applicationpage/HomePage.java
+++ app/src/main/java/liekkasnote/applicationpage/HomePage.java
@@ -1,38 +1,15 @@
 package liekkasnote.applicationpage;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.ActionMenuItemView;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.example.sk2014.liekkasnote.R;
 
 public class HomePage extends AppCompatActivity {
-    public ImageView diaryArrow;
-    public ImageView todoArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
-        findViews();
-        setClickEvent();
-    }
-
-    public void setClickEvent() {
-        diaryArrow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HomePage.this, DiaryList.class));
-            }
-        });
-    }
-
-    public void findViews() {
-        diaryArrow = (ImageView)findViewById(R.id.diary_arrow);
-        todoArrow = (ImageView)findViewById(R.id.todo_arrow);
     }
 }
