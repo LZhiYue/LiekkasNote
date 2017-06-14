dfae0cb743fda531be0f9598c94edd396a0d2e16
LZhiYue <q2912982@hotmail.com>
2017-06-14 22:23:21 +0800

    回滚

--- app/src/main/java/com/example/sk2014/liekkasnote/MainActivity.java
+++ app/src/main/java/com/example/sk2014/liekkasnote/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // 测试
         AVObject testObject = new AVObject("TestObject");
         testObject.put("words","Kongwan");
