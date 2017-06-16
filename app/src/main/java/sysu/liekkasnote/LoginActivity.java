4616164748c4c261068f15a3671b4565e36bca5a
LZhiYue <q2912982@hotmail.com>
2017-06-16 22:56:19 +0800

    修改UI界面

---
+++ app/src/main/java/sysu/liekkasnote/LoginActivity.java
@@ -0,0 +1,29 @@
+package sysu.liekkasnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.SaveCallback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        AVObject testObject = new AVObject("TestObject");
+        testObject.put("words","123456789");
+        testObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                if(e == null){
+                    Log.d("saved","success!");
+                }
+            }
+        });
+    }
+}
